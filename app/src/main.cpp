ea1ed04e33a00aba907169d297e0d872130066c9
Kilian Lütkemeyer <kilian@luetkemeyer.com>
2013-11-06 09:38:03 +0100

    used repeated start in example app

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,7 +6,7 @@ int main(int charc, char **charv) {
 	
 	pi::I2C_Device *dev = new pi::I2C_Device(0x60);
 	uint8_t error = 0;
-	uint8_t status = dev->read_reg_byte(0x00, &error);
+	uint8_t status = dev->read_reg_byte_rs(0x00, &error);
 	
 	
 	printf("status: 0x%02x [%x]\n", status, error);
