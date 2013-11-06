0e723a247cf77eaf0bb07dadc11786b046b54410
Kilian Lütkemeyer <kilian@luetkemeyer.com>
2013-11-06 10:07:51 +0100

    Added check for root previleges

--- lib/src/pi/pibcm2835.cpp
+++ lib/src/pi/pibcm2835.cpp
@@ -1,10 +1,20 @@
 #include "pi/pibcm2835.hpp"
 #include "bcm2835.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <cstdio>
+#include <cstdlib>
 
 namespace pi {
 
 	void BCM2835::init() 
 	{
+		uid_t uid = getuid();
+		if (uid != 0) {
+			fprintf(stderr, "Failed to start IO connection\nForgotten to run the application as root?...\n\n");
+			std::exit(1);
+		}
+		
 		bcm2835_init();
 	}
 
